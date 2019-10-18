1f0571dfa398f38298dad2b2fcbfee9c6f6dfe4c
Victor Martins <victorfrontend@gmail.com>
2019-10-18 00:54:37 -0300

    feat: add file, appointment and provider routes

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,16 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProviderController from './app/controllers/ProviderController';
+import FileController from './app/controllers/FileController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.create);
 routes.post('/sessions', SessionController.create);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.list);
+
+routes.post('/appointments', AppointmentController.create);
+
+routes.post('/files', upload.single('file'), FileController.create);
 
 export default routes;
