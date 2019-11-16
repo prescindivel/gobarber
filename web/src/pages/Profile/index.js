a874ae5da4db0c46fb550850c576ae1df4f17678
Victor Martins <victorfrontend@gmail.com>
2019-11-16 20:43:53 -0300

    feat(Routes): configure routes

---
+++ web/src/pages/Profile/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Profile = () => <div />;
+
+export default Profile;
---
+++ web/src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/profile" component={Profile} />
+  </Switch>
+);
+
+export default Routes;
