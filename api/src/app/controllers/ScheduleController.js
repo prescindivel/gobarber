9de2db0196182f8956c9920bf5359e9b5ac8aeab
Victor Martins <victorfrontend@gmail.com>
2019-11-16 19:28:04 -0300

    fix(Schedule): return user when list schedules

--- api/src/app/controllers/ScheduleController.js
+++ api/src/app/controllers/ScheduleController.js
@@ -25,6 +25,11 @@ class ScheduleController {
           [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
         }
       },
+      include: {
+        model: User,
+        as: 'user',
+        attributes: ['name']
+      },
       order: ['date']
     });
 
